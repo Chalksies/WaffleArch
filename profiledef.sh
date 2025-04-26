7eff0fcdae4cef1c88833064fc1ff7ed83306cbe
Chalksies <mooncarbonate@proton.me>
2025-04-26 22:12:42 +0300

    Boots into Plasma

--- profiledef.sh
+++ profiledef.sh
@@ -1,27 +1,34 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="WaffleOS"
-iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Chalkie <https://www.github.com/Chalksies>"
-iso_application="WaffleOS Live/Rescue DVD"
-iso_version="0.3"
+iso_name="WaffleOS-Plasma"
+iso_label="AUIKD"
+iso_publisher="Chalkie"
+desktop="Kde"
+iso_application="WaffleOS ${desktop} Live/Rescue medium"
+iso_version="0.4"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-ia32.systemd-boot.esp' 'uefi-x64.systemd-boot.esp'
-           'uefi-ia32.systemd-boot.eltorito' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=('bios.syslinux.mbr'
+           'bios.syslinux.eltorito'
+           'uefi-ia32.systemd-boot.eltorito'
+           'uefi-ia32.systemd-boot.esp'
+           'uefi-x64.systemd-boot.esp'
+           'uefi-x64.systemd-boot.eltorito'
+           )
+ia32_uefi_default_bootloader="uefi-ia32.systemd-boot.esp"
+x64_uefi_default_bootloader="uefi-x64.systemd-boot.esp"
 arch="x86_64"
 pacman_conf="pacman.conf"
+pacman_testing_conf="pacman-testing.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
-bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
+airootfs_image_tool_options=('-comp' 'zstd')
+bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
-  ["/root/.automated_script.sh"]="0:0:755"
+  ["/root/.automated_script.sh"]="0:0:750"
   ["/root/.gnupg"]="0:0:700"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
 )
